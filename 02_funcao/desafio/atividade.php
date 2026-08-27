78756a9b6039dcdfcab9b3a7f631c5686be406d6
ANA LARA DIAS APARECIDO <ana.aparecido@portalsesisp.org.br>
2026-08-27 13:47:36 -0300

    27/08/2026 13:47

---
+++ 02_funcao/desafio/atividade.php
@@ -0,0 +1,11 @@
+<?php
+require_once "funcao.php";
+
+$resultado = calcularPedido("Mouse",100,10,5,7);
+echo"Produto: ".$resultado["nomeProduto"]."<br>";
+echo"Subtotal: R$".$resultado["subtotal"]."<br>";
+echo"Desconto: R$".$resultado["valorDesconto"]."<br>";
+echo"Imposto: R$".$resultado["valorImposto"]."<br>";
+echo"Valor final: R$".$resultado["total"]."<br>";
+
+?>
